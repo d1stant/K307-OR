cc04a0ec22ff43eae80d90a705044207fe7abe65
Lcc Desktop <lcc@desktop.com>
2025-04-14 00:19:25 +0800

    更新环境

--- core/path_config.py
+++ core/path_config.py
@@ -2,7 +2,5 @@ from pathlib import Path
 
 cache_dir = Path("cache")
 data_dir = Path("data")
-js_dir = Path("js")
 cache_dir.mkdir(exist_ok=True)
 data_dir.mkdir(exist_ok=True)
-js_dir.mkdir(exist_ok=True)
